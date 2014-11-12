fd0cad4928dfa3df585587bd04166cbb0afe921a
vrxfile <vrxfile@mail.ru>
2014-11-12 18:07:31 +0300

    Timer B change type from cont to up

--- Trik_Devices/trik_async.c
+++ Trik_Devices/trik_async.c
@@ -13,11 +13,21 @@ void enableTimer_B()
 {
     //Overflow interrupt frequency is 366,21093755 Hz
     TIMER_B_clearTimerInterruptFlag(TIMER_B0_BASE);
+    /*
     TIMER_B_startContinuousMode(TIMER_B0_BASE,
             TIMER_B_CLOCKSOURCE_SMCLK,
             TIMER_B_CLOCKSOURCE_DIVIDER_1,
             TIMER_B_TBIE_INTERRUPT_ENABLE,
             TIMER_B_DO_CLEAR);
+            */
+    TIMER_B_startUpMode(TIMER_B0_BASE,
+            TIMER_B_CLOCKSOURCE_SMCLK,
+            TIMER_B_CLOCKSOURCE_DIVIDER_1,
+            0x1000,
+            TIMER_B_TBIE_INTERRUPT_ENABLE,
+            TIMER_B_CAPTURECOMPARE_INTERRUPT_DISABLE,
+            TIMER_B_DO_CLEAR);
+
 }
 
 void disableTimer_B()
