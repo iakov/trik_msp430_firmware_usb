ec7030d3d93bff4d2baac42683e1b258603d73f3
vrxfile <vrxfile@mail.ru>
2014-11-13 12:12:33 +0300

    Changed mode for timer B. Now freq is 10 kHz

--- Trik_Devices/trik_async.c
+++ Trik_Devices/trik_async.c
@@ -13,29 +13,23 @@ void enableTimer_B()
 {
     //Overflow interrupt frequency is 366,21093755 Hz
     TIMER_B_clearTimerInterruptFlag(TIMER_B0_BASE);
-    /*
-    TIMER_B_startContinuousMode(TIMER_B0_BASE,
-            TIMER_B_CLOCKSOURCE_SMCLK,
-            TIMER_B_CLOCKSOURCE_DIVIDER_1,
-            TIMER_B_TBIE_INTERRUPT_ENABLE,
-            TIMER_B_DO_CLEAR);
-            */
     TIMER_B_startUpMode(TIMER_B0_BASE,
             TIMER_B_CLOCKSOURCE_SMCLK,
             TIMER_B_CLOCKSOURCE_DIVIDER_1,
-            0x1000,
+            0x0960,
             TIMER_B_TBIE_INTERRUPT_ENABLE,
             TIMER_B_CAPTURECOMPARE_INTERRUPT_DISABLE,
             TIMER_B_DO_CLEAR);
-
 }
 
 void disableTimer_B()
 {
-    TIMER_B_startContinuousMode(TIMER_B0_BASE,
+    TIMER_B_startUpMode(TIMER_B0_BASE,
             TIMER_B_CLOCKSOURCE_SMCLK,
             TIMER_B_CLOCKSOURCE_DIVIDER_1,
+            0xFFFF,
             TIMER_B_TBIE_INTERRUPT_DISABLE,
+            TIMER_B_CAPTURECOMPARE_INTERRUPT_DISABLE,
             TIMER_B_DO_CLEAR);
     TIMER_B_stop(TIMER_B0_BASE);
     TIMER_B_clearTimerInterruptFlag(TIMER_B0_BASE);
