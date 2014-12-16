eba822465a1660cb7c8269814558738fc224cd50
vrxfile <vrxfile@mail.ru>
2014-12-16 19:03:35 +0300

    [tst1.py] Added function to write 16bit register

--- testfiles/tst1.py
+++ testfiles/tst1.py
@@ -1,4 +1,9 @@
-import math
+def write_16bit_reg(devaddr, funcnum, regaddr, regval):
+    crc = 0xFF - (devaddr + funcnum + regaddr + (regval & 0xFF) + ((regval >> 8) & 0xFF)) + 1
+    stmp = ":%02X%02X%02X%04X%02X\n" % (devaddr, funcnum, regaddr, regval, crc)
+    print(stmp)
 
-print "Hello world!"
-print math.pi
+write_16bit_reg(0x2E,0x03,0x01,0x0064)
+
+
+
