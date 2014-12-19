d06d52254ec6df168258cebd728598f9f42fd168
vrxfile <vrxfile@mail.ru>
2014-12-19 11:36:23 +0300

    [Python Trik Test] Added timer module

---
+++ testfiles/trik_timer.py
@@ -0,0 +1,14 @@
+__author__ = 'Rostislav Varzar'
+
+import trik_protocol
+
+# Timer address
+timer1 = 0x2E
+
+# Timer registers
+atctl = 0x00
+atper = 0x01
+atval = 0x02
+
+# ATCTL bits
+at_en = 0x0003
