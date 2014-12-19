a13dd029b77f20f7a9ab8995ac7bf64e2ef0bf48
vrxfile <vrxfile@mail.ru>
2014-12-19 16:00:28 +0300

    [Python Trik Test] Changed initialization of STTY

--- testfiles/trik_stty.py
+++ testfiles/trik_stty.py
@@ -5,5 +5,8 @@ import trik_protocol
 
 # Init Serial TTY device
 def init_stty():
-    sty1 = "stty -F %s -echo -onlcr" % (trik_protocol.fname1)
+    sty1 = "stty 921600 -F %s -echo -onlcr" % (trik_protocol.fname1)
     os.system(sty1)
+    sty1 = "stty 921600 -F %s -echo -onlcr" % (trik_protocol.fname2)
+    os.system(sty1)
+
