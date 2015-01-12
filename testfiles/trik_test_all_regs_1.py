f373f7245f1e4ed733c43a76415b96da7347c1a7
vrxfile <vrxfile@mail.ru>
2015-01-12 18:42:23 +0300

    [Python Trik Test] Added enumeration of all values in 3 cycles

--- testfiles/trik_test_all_regs_1.py
+++ testfiles/trik_test_all_regs_1.py
@@ -5,6 +5,17 @@ import trik_protocol, trik_motor, trik_encoder, trik_timer, trik_sensor, trik_to
 import trik_stty, trik_power
 import random
 
+# Test register values
+testregvals = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+               0x0E, 0x0F, 0x10,
+               0xFE, 0xFF, 0x100,
+               0xFFE, 0xFFF, 0x1000,
+               0xFFFE, 0xFFFF, 0x10000,
+               0xFFFFE, 0xFFFFF, 0x100000,
+               0xFFFFFE, 0xFFFFFF, 0x1000000,
+               0xFFFFFFE, 0xFFFFFFF, 0x10000000,
+               0xFFFFFFFE, 0xFFFFFFFF]
+
 # Init Serial TTY device
 trik_stty.init_stty()
 
@@ -17,7 +28,24 @@ time.sleep(5)
 # Clear screen
 os.system("clear")
 
-# Test all device addresses and all register addresses and some registers values
-def stress_test():
-    step = 0
+# Test all device addresses and all register addresses and some registers values (writing mode)
+def stress_test_writing():
+    global testregvals
+    devaddr = 0x00
+    while devaddr <= 0xFF:
+        regaddr = 0x00
+        while regaddr <= 0xFF:
+            ridx = 0x00
+            while ridx < len(testregvals):
+                regval = testregvals[ridx]
+                stmp1 = "ADDR=0x%02X REG=0x%02X VAL=0x%08X" % (devaddr, regaddr, regval)
+                print stmp1
+
+
+
+                ridx = ridx + 1
+            regaddr = regaddr + 1
+        devaddr = devaddr + 1
+
+stress_test_writing()
 
