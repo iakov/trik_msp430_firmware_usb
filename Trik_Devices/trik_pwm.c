6b3f4f212bfaa4b7aab2b698627931b951dc044f
vrxfile <vrxfile@mail.ru>
2015-01-28 15:22:43 +0300

    [Trik MSP] Added header files for PWM module

---
+++ Trik_Devices/trik_pwm.c
@@ -0,0 +1,8 @@
+/*
+ * trik_pwm.c
+ *
+ *  Created on: January 28, 2015
+ *      Author: Rostislav Varzar
+ */
+
+
---
+++ Trik_Devices/trik_pwm.h
@@ -0,0 +1,13 @@
+/*
+ * trik_pwm.h
+ *
+ *  Created on: January 28, 2015
+ *      Author: Rostislav Varzar
+ */
+
+#ifndef TRIK_PWM_H_
+#define TRIK_PWM_H_
+
+
+
+#endif /* TRIK_PWM_H_ */
