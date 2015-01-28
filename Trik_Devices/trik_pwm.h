b40d90b2a18899b7afdaa97d7c96cff0f4a53b19
vrxfile <vrxfile@mail.ru>
2015-01-28 15:43:11 +0300

    [Trik MSP] Added PWM defines and prototypes

--- Trik_Devices/trik_pwm.h
+++ Trik_Devices/trik_pwm.h
@@ -8,6 +8,35 @@
 #ifndef TRIK_PWM_H_
 #define TRIK_PWM_H_
 
+//PWM registers
+#define PPCTL               0x00
+#define PPDUT               0x01
+#define PPPER               0x02
 
+//PWM errors
+#define PWM_NO_ERROR        0x00
+
+//Masks for bits of control register
+#define PWM_ENABLE          0x8000 //PWM enable
+
+//Max PWMs
+#define MAX_PWMS            0x05
+
+//Prototypes for the APIs
+void PWM_handler(uint8_t PWM_NUMBER);
+void PWM_enable(uint8_t PWM_NUMBER);
+void PWM_disable(uint8_t PWM_NUMBER);
+
+//PWM registers
+struct tPWMRegisters
+{
+    uint16_t PCTL;
+    uint16_t PDUT;
+    uint16_t PPER;
+    uint32_t PERR;
+};
+
+//Motors registers array
+volatile struct tPWMRegisters PWM[MAX_PWMS];
 
 #endif /* TRIK_PWM_H_ */
