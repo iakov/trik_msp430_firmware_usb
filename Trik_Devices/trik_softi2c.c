5342b9576022e44642226fc674e4d5f2ec7b3d08
vrxfile <vrxfile@mail.ru>
2015-02-04 17:32:27 +0300

    Added empty files for software i2c API

---
+++ Trik_Devices/trik_softi2c.c
@@ -0,0 +1,8 @@
+/*
+ * trik_softi2c.c
+ *
+ *  Created on: Fabruary 4, 2015
+ *      Author: Rostislav Varzar
+ */
+
+
---
+++ Trik_Devices/trik_softi2c.h
@@ -0,0 +1,13 @@
+/*
+ * trik_softi2c.h
+ *
+ *  Created on: Fabruary 4, 2015
+ *      Author: Rostislav Varzar
+ */
+
+#ifndef TRIK_SOFTI2C_H_
+#define TRIK_SOFTI2C_H_
+
+
+
+#endif /* TRIK_SOFTI2C_H_ */
