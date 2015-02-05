a5566073fadac62b60025ca0640b3a629d4b9823
vrxfile <vrxfile@mail.ru>
2015-02-05 16:17:40 +0300

    Added functions to change pins states

--- Trik_Devices/trik_softi2c.c
+++ Trik_Devices/trik_softi2c.c
@@ -8,4 +8,65 @@
 #include <stdint.h>
 #include <msp430f5510.h>
 #include "trik_softi2c.h"
+#include "trik_devices.h"
 
+void I2C_delay(uint16_t i2c_del)
+{
+    do
+    {
+        _nop();
+    }
+    while(--i2c_del);
+}
+
+void I2C_lowSDA(uint8_t I2C_NUMBER)
+{
+    switch (I2C_NUMBER)
+    {
+        case I2C1:
+            I2C_DIR1 |= SDA1;
+            I2C_WAIT
+            break;
+        default:;
+        break;
+    }
+}
+
+void I2C_highSDA(uint8_t I2C_NUMBER)
+{
+    switch (I2C_NUMBER)
+    {
+        case I2C1:
+            I2C_DIR1 &= ~SDA1;
+            I2C_WAIT
+            break;
+        default:;
+        break;
+    }
+}
+
+void I2C_lowSCL(uint8_t I2C_NUMBER)
+{
+    switch (I2C_NUMBER)
+    {
+        case I2C1:
+            I2C_OUT1 &= ~SCL1;
+            I2C_WAIT
+            break;
+        default:;
+        break;
+    }
+}
+
+void I2C_highSCL(uint8_t I2C_NUMBER)
+{
+    switch (I2C_NUMBER)
+    {
+        case I2C1:
+            I2C_OUT1 |= SCL1;
+            I2C_WAIT
+            break;
+        default:;
+        break;
+    }
+}
