376b2257abf85ec1d5fd8e6b13388c12bd9ffd9b
vrxfile <vrxfile@mail.ru>
2015-02-24 19:34:31 +0300

    Added registers addresses for versions of modules

--- Trik_Devices/trik_version.h
+++ Trik_Devices/trik_version.h
@@ -9,8 +9,8 @@
 #define TRIK_VERSION_H_
 
 // Modules versions
-#define MAIN_VERSION        0x0025
 #define MAIN_VERSION_TXT    "0.0.25\n\0"
+#define MAIN_VERSION        0x0025
 #define MOTOR_VERSION       0x0025
 #define SENSOR_VERSION      0x0025
 #define ENCODER_VERSION     0x0025
@@ -21,5 +21,17 @@
 #define I2C_VERSION         0x0025
 #define BSL_VERSION         0x0025
 
+// Version registers adresses defines
+#define MAIN_VER_REG_1      0x00
+#define MAIN_VER_REG_2      0x01
+#define MOT_VER_REG         0x02
+#define SENS_VER_REG        0x03
+#define ENC_VER_REG         0x04
+#define PORT_VER_REG        0x05
+#define PWM_VER_REG         0x06
+#define ATMR_VER_REG        0x07
+#define TOUCH_VER_REG       0x08
+#define I2C_VER_REG         0x09
+#define BSL_VER_REG         0x0A
 
 #endif /* TRIK_VERSION_H_ */
