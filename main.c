4ab03b9a8f93f97f12b03a249ac0ef34a532de60
vrxfile <vrxfile@mail.ru>
2015-05-27 16:56:45 +0300

    Some problems with testing URM04

--- main.c
+++ main.c
@@ -124,16 +124,9 @@ void main (void)
 
    __enable_interrupt();  // Enable interrupts globally
 
+
    I2C_init(I2C4);
    USART_reset(USART4);
-   USART_set_speed(USART4, 50);
-   USART_config(USART4, USART_8BITS + USART_RS485 + USART_INVRTS + USART_RXEN + USART_TXEN);
-   uint8_t cfg1 = USART_read_reg(USART4, 0x03);
-   USART_write_reg(USART4, 0x03, cfg1 | 0x80);
-   uint8_t dd0 = USART_read_reg(USART4, 0x00);
-   uint8_t dd1 = USART_read_reg(USART4, 0x01);
-   USART_write_reg(USART4, 0x03, cfg1);
-
 
     while (1)
     {
@@ -177,10 +170,39 @@ void main (void)
                         }
                     }
                     */
+
+                      USART_set_speed(USART4, 19200);
+                      USART_config(USART4, USART_8BITS + USART_RS485 + USART_INVRTS + USART_RXEN + USART_TXEN);
+                      uint8_t cfg1 = USART_read_reg(USART4, 0x03);
+                      USART_write_reg(USART4, 0x03, cfg1 | 0x80);
+                      uint8_t dd0 = USART_read_reg(USART4, 0x00);
+                      uint8_t dd1 = USART_read_reg(USART4, 0x01);
+                      USART_write_reg(USART4, 0x03, cfg1);
+
                     USART_transmit_byte(USART4, 0x55);
+                    USART_transmit_byte(USART4, 0xAA);
+                    USART_transmit_byte(USART4, 0x12);
+                    USART_transmit_byte(USART4, 0x00);
+                    USART_transmit_byte(USART4, 0x03);
+                    USART_transmit_byte(USART4, 0x55+0xAA+0x12+0x00+0x03);
+                    /*
                     sprintf(newString, "%x %x %x %x %x %x %x %x %x %x\n", USART_read_reg(USART4, 0x02), USART_read_reg(USART4, 0x03),
                     		USART_read_reg(USART4, 0x0F), dd1, dd0, USART_is_data_in_buffer(USART4), USART_read_reg(USART4, 0x05),
 							USART_receive_byte(USART4), USART_receive_byte(USART4), USART_receive_byte(USART4));
+					*/
+                    sprintf(newString, "%x %x %x %x %x %x %x %x %x %x %x %x\n",
+                    		USART_is_data_in_buffer(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_receive_byte(USART4),
+							USART_is_data_in_buffer(USART4));
 
 
                     if (cdcSendDataInBackground((uint8_t*)newString,
