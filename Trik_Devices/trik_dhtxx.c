6311083a23698cb2497bf25a71312e263a9046a3
vrxfile <vrxfile@mail.ru>
2015-07-27 18:19:01 +0300

    Fixed all bugs and tested DHT22

--- Trik_Devices/trik_dhtxx.c
+++ Trik_Devices/trik_dhtxx.c
@@ -10,13 +10,15 @@
 #include "trik_dhtxx.h"
 #include "trik_sensor.h"
 #include "trik_devices.h"
+#include "trik_async.h"
 
-uint16_t DHT_read(uint8_t SENS_NUMBER, uint8_t* out_data)
+uint8_t DHT_read(uint8_t SENS_NUMBER, uint8_t* out_data)
 {
-
 	const uint8_t *end_data = out_data + 6;
-	register uint16_t stmp, etmp;
 	register uint8_t tmp1 = 1;
+	register uint16_t cnt1 = 0;
+	register uint16_t cnt2 = 0;
+	register uint16_t cnt3 = 0;
 
 	out_data[0] = out_data[1] = out_data[2] = out_data[3] = out_data[4] = out_data[5] = 0;
 	switch (SENS_NUMBER)
@@ -26,38 +28,41 @@ uint16_t DHT_read(uint8_t SENS_NUMBER, uint8_t* out_data)
 			P6OUT &= ~BIT5;
 			P6DIR |= BIT5;
 			P6REN &= ~BIT5;
-			stmp = TB0R;
-			while((TB0R - stmp) < 54000)
-				;  // Wait 18ms
+			// Wait 18ms for startup
+			__delay_cycles(WAIT_18_MS);
 			P6REN |= BIT5;
 			P6OUT |= BIT5;
 			P6DIR &= ~BIT5;
 			// Wait 20-40us + 80us for response
-			stmp = TB0R;
+			cnt1 = 0;
 			while (P6IN & BIT5)
 			{
-				if ((TB0R - stmp) > 300)
+				__delay_cycles(WAIT_1_US); cnt1 ++;
+				if (cnt1 > 100)
 					return DHT_NO_RESPONSE;
 			}
 			// Read data
-			etmp = TB0R;
 			do {
+				cnt1 = cnt2 = 0;
+				cnt3++;
 				// Wait 80us until DHT pullups
-				stmp = etmp;
 				while (!(P6IN & BIT5))
 				{
-					if ((TB0R - stmp) > 300)
+					__delay_cycles(WAIT_1_US);
+					cnt1 ++;
+					if (cnt1 > 100)
 						return DHT_ERROR_80us;
 				}
 				// Wait transmission
 				while (P6IN & BIT5)
 				{
-					if ((TB0R - stmp) > 600)
+					__delay_cycles(WAIT_1_US);
+					cnt2 ++;
+					if (cnt2 > 100)
 						return DHT_TRANS_ERROR;
 				}
-				// Get end time
-				etmp = TB0R;
-				if ((etmp - stmp) > 330) // Logic "one", if signal > 110us
+
+				if ((cnt1 + cnt2) > 110) // Logic "one", if signal > 110us
 					*out_data |= tmp1;
 				if (!(tmp1 >>= 1))
 				{
@@ -77,15 +82,39 @@ uint16_t DHT_read(uint8_t SENS_NUMBER, uint8_t* out_data)
 	return DHT_NO_ERROR;
 }
 
-uint16_t DHT_getTemp(uint8_t SENS_NUMBER)
+uint32_t DHT_getTemp(uint8_t SENS_NUMBER)
 {
 	uint8_t dht_data[6];
-	return DHT_read(SENS_NUMBER, dht_data);
+	uint8_t dht_result;
+	disableTimer_B();
+	dht_result = DHT_read(SENS_NUMBER, dht_data);
+	enableTimer_B();
+	if (dht_result)
+	{
+		return DHT_MAIN_ERROR;
+	}
+	else
+	{
+		return ((uint16_t)(dht_data[3] << 8) | dht_data[4]);
+	}
+
 }
 
-uint16_t DHT_getHum(uint8_t SENS_NUMBER)
+uint32_t DHT_getHum(uint8_t SENS_NUMBER)
 {
 	uint8_t dht_data[6];
-	return DHT_read(SENS_NUMBER, dht_data);
+	uint8_t dht_result;
+	disableTimer_B();
+	dht_result = DHT_read(SENS_NUMBER, dht_data);
+	enableTimer_B();
+	if (dht_result)
+	{
+		return DHT_MAIN_ERROR;
+	}
+	else
+	{
+		return ((uint16_t)(dht_data[1] * 256) + dht_data[2]);
+	}
+
 }
 
--- Trik_Devices/trik_dhtxx.h
+++ Trik_Devices/trik_dhtxx.h
@@ -8,6 +8,7 @@
 #ifndef TRIK_DEVICES_TRIK_DHTXX_H_
 #define TRIK_DEVICES_TRIK_DHTXX_H_
 
+// Defines for DHTxx errors
 #define DHT_NO_ERROR		0x00
 #define DHT_NO_RESPONSE		0x01
 #define DHT_ERROR_80us		0x02
@@ -15,9 +16,19 @@
 #define DHT_NO_START		0x04
 #define DHT_BAD_CRC			0x05
 #define DHT_SENS_NUM		0xFF
+#define DHT_MAIN_ERROR		0xFFFFFFFF
 
-uint16_t DHT_read(uint8_t SENS_NUMBER, uint8_t* out_data);
-uint16_t DHT_getTemp(uint8_t SENS_NUMBER);
-uint16_t DHT_getHum(uint8_t SENS_NUMBER);
+// Defines for time delays, 1 us = 14.4 cycles
+#define WAIT_1_US			14
+#define WAIT_10_US			144
+#define WAIT_100_US			1440
+#define WAIT_110_US			1584
+#define WAIT_200_SS			2880
+#define WAIT_18_MS			259200
+
+
+uint8_t DHT_read(uint8_t SENS_NUMBER, uint8_t* out_data);
+uint32_t DHT_getTemp(uint8_t SENS_NUMBER);
+uint32_t DHT_getHum(uint8_t SENS_NUMBER);
 
 #endif /* TRIK_DEVICES_TRIK_DHTXX_H_ */
