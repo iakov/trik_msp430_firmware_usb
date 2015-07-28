ba43e5a53304f036819b17c8f3e6ce0a808368ea
vrxfile <vrxfile@mail.ru>
2015-07-28 15:35:19 +0300

    Changed main version

--- Trik_Devices/trik_version.h
+++ Trik_Devices/trik_version.h
@@ -9,8 +9,8 @@
 #define TRIK_VERSION_H_
 
 // Modules versions
-#define MAIN_VERSION_TXT    "0.0.36\n\0"
-#define MAIN_VERSION        36
+#define MAIN_VERSION_TXT    "0.0.37\n\0"
+#define MAIN_VERSION        37
 #define MOTOR_VERSION       3
 #define SENSOR_VERSION      3
 #define ENCODER_VERSION     2
